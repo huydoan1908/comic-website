5407b94570a457735348136f87cd1d56887ab4ed
Huy Doan <huy11900@gmail.com>
2025-10-21 12:50:08 +0700

    Update ComicReader.tsx

--- src/components/ComicReader.tsx
+++ src/components/ComicReader.tsx
@@ -105,8 +105,8 @@ export function ComicReader({ chapters, chapterNumber = 1, comicTitle, comicId }
         </div>
       ) : (
         /* Continuous View */
-        <div className="flex flex-col items-center space-y-6 px-4 mt-20 mb-16">
-          <div className="flex flex-col items-center space-y-6 w-full">
+        <div className="px-4 mt-20 mb-16">
+          <div className="flex flex-col items-center w-full">
             {/* Chapter Pages */}
             {currentChapter.pageImageUrls.map((pageUrl, pageIndex) => (
               <div key={`${currentChapter.id}-${pageIndex}`} className="relative w-full max-w-4xl">
